690ed912776e2ffd9da2274837c131fe55237a8a
Luis Inacio <lrs.inacio@gmail.com>
2022-04-29 18:21:37 +0100

    Topic and Forum Areas def and home page done

---
+++ Forum.Web/Areas/Forum/Controllers/TopicController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Web.Areas.Forum.Controllers
+{
+    [Area("Forum")]
+    public class TopicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
