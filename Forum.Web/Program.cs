1082338aa2e80495092daf30b658bf118e33d4df
Luis Inacio <lrs.inacio@gmail.com>
2022-04-30 02:26:18 +0100

    500 user Error page

--- Forum.Web/Program.cs
+++ Forum.Web/Program.cs
@@ -18,9 +18,10 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Forum/Home/Error");
     app.UseHsts();
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
--- Forum.Web/ViewModels/TopicVm.cs
+++ Forum.Web/ViewModels/TopicVm.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-using Microsoft.Build.Framework;
 
 namespace Forum.Web.ViewModels;
 
@@ -7,11 +6,8 @@ public class TopicVm
 {
     [ValidateNever]
     public Guid Id { get; set; }
-    [Required]
     public string? Title { get; set; }
-    [Required]
     public string? Description { get; set; }
-
     [ValidateNever]
     public DateTime CreationDate { get; set; }
 }
